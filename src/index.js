cc7256e0452d83e65bf392eab407672b11f7ffb2
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 11:21:34 -0500

    Serves imported  files

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ The "distribution" code is the minimized and optimized output
  of our build process that will eventually be loaded in the browser
 */
 
-import './style.css'
+import './style.css';
+import HappyFace from './hf.png';
 
 function component() {
   var element = document.createElement('div');
-
   element.innerHTML = (`<h2>Hello Webpack!</h2>`)
   element.classList.add('title');
+
+  var hf = new Image();
+  hf.src = HappyFace;
+  element.appendChild(hf);
 
   return element;
 }
