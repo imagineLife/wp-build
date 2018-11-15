a216368c08a0c8707620d30bd4c831465d67f21e
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 10:51:40 -0500

    config file

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+/*
+	If a webpack.config.js is present, the webpack command picks it up by default.
+*/
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
