98d5c786b607907b17dd8119e3c571c3cc68fcdc
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 10:40:07 -0500

    Includes dist folder & dist index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*
+The "source" code is the code that we'll write and edit. 
+The "distribution" code is the minimized and optimized output
+ of our build process that will eventually be loaded in the browser
+*/
+
 function component() {
   let element = document.createElement('div');
 
