9cbdcc3797c1032e37212bb88d033962046427c7
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 10:57:28 -0500

    begins ability to load css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 /*
 	If a webpack.config.js is present, the webpack command picks it up by default.
+	LOADING CSS
+		webpack uses a regular expression
+		 to determine which files it should look for
+		  and serve to a specific loader.
+		 In this case any file that ends with .css
+		  will be served to the style-loader
+		   and the css-loader.
 */
 
 const path = require('path');
@@ -9,5 +16,16 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+  	rules: [
+  		{
+  			test: /\.css$/,
+  			use: [
+  				'style-loader',
+  				'css-loader'
+  			]
+  		}
+  	]
   }
 };
