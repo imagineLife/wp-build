e0477ce35afee22f6f1e0e00f4bc9ec04892decb
Eric Laursen <ericjlaursen@gmail.com>
2018-11-15 11:05:00 -0500

    converts main to bundle, adds css class & stylesheet

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ The "distribution" code is the minimized and optimized output
  of our build process that will eventually be loaded in the browser
 */
 
+import './style.css'
+
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = (`<h2>Hello Webpack!</h2>`)
+  element.classList.add('title');
 
   return element;
 }
